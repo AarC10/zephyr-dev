18b151686098abd7c53aafe15288994e087ef0e5
Aaron Chan <aarchan108@gmail.com>
2023-12-18 01:08:06 -0500

    Check device id functionality for ADXL375

--- drivers/sensor/adxl375/adxl375.c
+++ drivers/sensor/adxl375/adxl375.c
@@ -21,51 +21,80 @@
 
 LOG_MODULE_REGISTER(ADXL375, CONFIG_SENSOR_LOG_LEVEL);
 
-static int adxl375_init() {
+static int adxl375_check_id(const struct device *dev) {
+	struct adxl375_data *data = dev->data;
+	uint8_t device_id = 0;
 
-
-}
-
-static int adxl375_sample_fetch(const struct device *dev, enum sensor_channel chan) {
-
-	return -1;
-}
-
-static int adxl375_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val) {
-	return -1;
-}
-
-static const struct sensor_driver_api adxl375_driver_api = {
-	.sample_fetch = adxl375_sample_fetch,
-	.channel_get = adxl375_channel_get
-}
-
-
-static int adxl375_check_id() {
-	int ret = 0; // TODO: read reg
+	int ret = data->hw_tf->read_reg(dev, ADXL375_DEVID, &device_id);
 
 	if (ret != 0) {
+		return ret;
+	}
+
+	if (ADXL375_DEVID_VAL != device_id) {
 		return -ENODEV;
 	}
 
 	return 0;
 }
 
-static int adxl375_set_odr() {
+static int adxl375_set_odr()
+{
+
 
 }
 
-static int adxl375_set_op_mode() {
+static int adxl375_set_op_mode()
+{
 
 }
 
-int adxl375_init(const struct device *dev) {
+static int adxl375_wakeup()
+{
+
+}
+
+static int adxl375_init()
+{
+
+	int ret;
+	const struct adxl375_dev_config *cfg = dev->config;
+
+	ret = cfg->bus_init(dev);
+	if (ret < 0) {
+		LOG_ERR("Failed to initialzie sensor bus.");
+		return ret;
+	}
+
 	int ret = adxl375_check_id();
 	if (!ret) {
 		return ret;
 	}
 
+
+	ret = adxl375_set_odr();
+
+	ret = adxl375_set_op_mode();
+
+	ret = adxl375_wakeup();
+
+	return -1;
+
+}
+
+static int adxl375_sample_fetch(const struct device *dev, enum sensor_channel chan)
+{
+
 	return -1;
 }
 
+static int adxl375_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+{
+	return -1;
+}
 
+static const struct sensor_driver_api adxl375_driver_api =
+{
+	.sample_fetch = adxl375_sample_fetch,
+	.channel_get = adxl375_channel_get
+};
